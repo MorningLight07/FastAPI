a8efa3be53d5346965bec0323f466a0c7318c44b
Alec <joshuayumague07@gmail.com>
2024-03-27 23:35:09 +0800

    last Wednesday change. added, schema validation

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from typing import Optional
 from fastapi import Body, FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
@@ -13,7 +21,8 @@ def get_posts():
     return {"data": "This is your posts"}
 
 @app.post("/createposts")
-def create_posts(body: dict = Body(...)):
-    print(body)
-    return {"new_post": f"title {body['title']} content: {body['content']}"}
+def create_posts(post:Post):
+    print(post)
+    print(post.dict())
+    return {"data":"post"}
 
